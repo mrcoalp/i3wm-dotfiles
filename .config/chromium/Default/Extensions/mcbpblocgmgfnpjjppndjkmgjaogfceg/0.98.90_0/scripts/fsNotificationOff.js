0c1bb561d11cc3ffe0c732e8a0dafecee3e2f860
mrcoalp <marco.alp120@gmail.com>
2016-10-09 02:04:26 +0100

    first config

--- .config/chromium/Default/Extensions/mcbpblocgmgfnpjjppndjkmgjaogfceg/0.98.90_0/scripts/fsNotificationOff.js
+++
@@ -1,6 +0,0 @@
-//********************************************************************************************************
-// FireShot - Webpage Screenshots and Annotations
-// Copyright (C) 2007-2016 Evgeny Suslikov (evgeny@suslikov.ru)
-//********************************************************************************************************
-
-document.body.removeChild(document.getElementById('FireShotNotification'));
--- .config/chromium/Default/Extensions/mcbpblocgmgfnpjjppndjkmgjaogfceg/0.98.90_0/scripts/fsScriptChecker.js
+++
@@ -1,8 +0,0 @@
-//********************************************************************************************************
-// FireShot - Webpage Screenshots and Annotations
-// Copyright (C) 2007-2016 Evgeny Suslikov (evgeny@suslikov.ru)
-//********************************************************************************************************
-
-var scriptLoaded;
-
-chrome.runtime.sendMessage({message:(typeof scriptLoaded == "undefined" ? "loadScript" : "execScript")});
